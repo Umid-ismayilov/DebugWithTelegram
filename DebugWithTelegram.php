22dacaca4601f6cb62e00abd67790e8d6024a332
Umid Ismayilov <umid.ismayilov01@gmail.com>
2022-06-02 09:55:27 +0400

    DebugWithTelegram

---
+++ DebugWithTelegram.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+class DebugWithTelegram
+{
+    private $token =  'your-token';
+
+    public function debug($data){
+        $info = [
+            'chat_id' => 'your-chat_id',
+            'text' => $data
+        ];
+        $params = http_build_query($info, '', '&');
+        $this->sendMessage($params);
+
+    }
+    private function sendMessage($params){
+        $curl = curl_init();
+        $url = "https://api.telegram.org/bot$this->token/".__FUNCTION__."?$params";
+        curl_setopt_array($curl, [CURLOPT_URL => $url]);
+        curl_exec($curl);
+        curl_close($curl);
+    }
+}
